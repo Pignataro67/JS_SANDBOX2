0bec999084d88db3e7785c2f9e49e2c0d1ebb6f2
Pignataro67 <Pignataro67@gmail.com>
2021-04-28 04:19:26 -0700

    variables and a function

--- app.js
+++ app.js
@@ -16,8 +16,23 @@ Game Function:
 
   // UI Elements
 const game = document.querySelector('#game'),
-        minNum = document.querySelector('.min-num'), 
-        maxNum = document.querySelector('.max-num'), 
-        guessBtn = document.querySelector('#guess-btn'), 
-        guessInput = document.querySelector('#guess-input'), 
-        message = document.querySelector('.message');
+      minNum = document.querySelector('.min-num'), 
+      maxNum = document.querySelector('.max-num'), 
+      guessBtn = document.querySelector('#guess-btn'), 
+      guessInput = document.querySelector('#guess-input'), 
+      message = document.querySelector('.message');
+
+// Assign UI min and max
+
+minNum.textContent = min;
+maxNum.textContent = max;
+
+// Listen for guess
+guessBtn.addEventListener('click', function() {
+  let guess = parseInt(guessInput.value);
+  console.log(guess);
+  // Validate
+//   if(){
+    
+//   }
+});
