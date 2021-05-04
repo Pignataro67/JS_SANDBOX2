337216631784d00bcff154721c90c8da2781acd8
Pignataro67 <Pignataro67@gmail.com>
2021-05-04 05:53:14 -0700

    prototypes

--- app.js
+++ app.js
@@ -1,47 +1,19 @@
-// String 
+// Object.prototype
+// Person.prototype
 
-const name1 = 'Jeff';
-const name2 = new String('Jeff');
-
-// name2.foo = 'bar';
-// console.log(name2);
-
-console.log(typeof name2);
-
-if(name2 === 'Jeff') {
-  console.log('YES');
-} else {
-  console.log('NO');
+//  Person Constructor 
+function Person(firstName, lastName,dob) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.birthday = new Date(dob);
+  this.getAge = function() {
+    const diff = Date.now() - this.birthday.getTime();
+    const ageDate = new Date(diff);
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+  }
 }
 
-// Number
-const num1 = 5;
-const num2 = new number(5);
+const john = new Person('John', 'Doe', '8-12-90');
+const mary = new Person('Mary', 'Johnson', 'March 20 1978');
 
-// Boolean
-const bool1 = true;
-const bool2 = new Boolean(true);
-
-// Funciton
-const getSum1 = function(x, y) {
-  return x + y;
-}
-
-const getSum2 = new Function('x', 'y', 'return 1 + 1');
-
-// console.log(getSum2(1, 1));
-
-// Object
-const john1 = {name: "John"};
-const john2 = new Object({name: "John"});
-console.log(john2);
-
-// Arrays
-const arr1 = [1, 2, 3, 4,];
-const arr2 = new Array(1, 2, 3, 4);
-
-// Regular Expressions
-const re1 = /\w+/;
-const re2 = new RegExp('\\w+');
-
-console.log(re2);
+console.log(mary);
