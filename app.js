2c47c8f39904034d47f5155943cd735a610c887d
Pignataro67 <Pignataro67@gmail.com>
2021-05-05 05:17:25 -0700

    more with prototypes

--- app.js
+++ app.js
@@ -6,14 +6,42 @@ function Person(firstName, lastName,dob) {
   this.firstName = firstName;
   this.lastName = lastName;
   this.birthday = new Date(dob);
-  this.getAge = function() {
-    const diff = Date.now() - this.birthday.getTime();
-    const ageDate = new Date(diff);
-    return Math.abs(ageDate.getUTCFullYear() - 1970);
-  }
+  // this.getAge = function() {
+  //   const diff = Date.now() - this.birthday.getTime();
+  //   const ageDate = new Date(diff);
+  //   return Math.abs(ageDate.getUTCFullYear() - 1970);
+  // }
+}
+
+// Calculate age
+Person.prototype.getAge = function() {
+  const diff = Date.now() - this.birthday.getTime();
+  const ageDate = new Date(diff);
+  return Math.abs(ageDate.getUTCFullYear() - 1970);
+}
+
+// Get full name
+Person.prototype.getFulllName = function() {
+  return `${this.firstName} ${this.lastName}`;
+}
+
+// Gets Married
+Person.prototype.getsMarried = function(newLastName) {
+  this.lastName = newLastName;
 }
 
 const john = new Person('John', 'Doe', '8-12-90');
 const mary = new Person('Mary', 'Johnson', 'March 20 1978');
 
 console.log(mary);
+
+console.log(john.getAge());
+
+console.log(mary.getFulllName());
+
+mary.getsMarried('Smith');
+
+console.log(mary.getFulllName());
+
+console.log(mary.hasOwnProperty('firstName'));
+console.log(mary.hasOwnProperty('getFullName'));
