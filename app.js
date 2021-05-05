10411d6c717b18ac60654ca22484cce610e399d9
Pignataro67 <Pignataro67@gmail.com>
2021-05-05 05:29:33 -0700

    prototypes and inheretance

--- app.js
+++ app.js
@@ -12,3 +12,18 @@ Person.prototype.greeting = function() {
 const person1 = new Person('John', 'Doe');
 
 // console.log(person1.greeting());
+
+// Customer constructor
+function Customer(firstName, lastName, phone, membership) {
+  Person.call(this, firstName, lastName);
+
+  this.phone = phone;
+  this.membership = membership;
+}
+
+// Create customer
+const customer1 = new Customer('Tom', 'Smith', '555-555-5555', 'Standard');
+
+console.log(customer1);
+
+console.log(customer1.greeting());
