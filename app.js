fbd08ece705b0f83976a657c5e492a4b3d1f01ee
Pignataro67 <Pignataro67@gmail.com>
2021-05-06 05:32:46 -0700

    using Object.create

--- app.js
+++ app.js
@@ -1,40 +1,12 @@
-// Person constructor
-function Person(firstName, lastName) {
-  this.firstName = firstName;
-  this.lastName = lastName;
+const personProtypes = {
+  greeting: function() {
+    return `Hello there ${this.firstName} ${this.lastName}`;
+  }
 }
 
-// Greeting
-Person.prototype.greeting = function() {
-  return `Hello there ${this.firstName} ${this.lastName}`;
-}
+const mary = Object.create(personProtypes);
+mary.firstName = 'Mary';
+mary.lastName = 'William';
+mary.age = 30;
 
-const person1 = new Person('John', 'Doe');
-
-console.log(person1.greeting());
-
-// Customer constructor
-function Customer(firstName, lastName, phone, membership) {
-  Person.call(this, firstName, lastName);
-
-  this.phone = phone;
-  this.membership = membership;
-}
-
-// Inherit the Person prototype methods
-Customer.prototype = Object.create(person.prototype);
-
-// New customer.prototype return Customer()
-Customer.prototype = Object.create(person.prototype);
-
-// Create customer
-const customer1 = new Customer('Tom', 'Smith', '555-555-5555', 'Standard');
-
-console.log(customer1);
-
-// Customer greeting
-Customer.prototype.greeting = function() {
-  return `Hello there ${this.firstName} ${this.lastName} Welcome to our company`;
-}
-
-console.log(customer1.greeting());
+console.log(mary.greeting());
