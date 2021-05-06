d35a167283c979c19b05152c7365832dc7c11ba0
Pignataro67 <Pignataro67@gmail.com>
2021-05-06 05:37:06 -0700

    more using Object.create

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const personProtypes = {
   greeting: function() {
     return `Hello there ${this.firstName} ${this.lastName}`;
+  },
+  getsMarried: function(newLastName) {
+    this.lastName = newLastName;
   }
 }
 
@@ -9,4 +12,16 @@ mary.firstName = 'Mary';
 mary.lastName = 'William';
 mary.age = 30;
 
+mary.getsMarried('Thompson');
+
 console.log(mary.greeting());
+
+const pat = Object.create(personProtypes, {
+  firstName: {value: 'Pat'},
+  lastName: {value: 'Debaadeo'},
+  age: {value: 42}
+});
+
+console.log(pat);
+
+console.log(pat.greeting());
