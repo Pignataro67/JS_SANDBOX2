7529351f1b828c96efd28f29e49faea1f22ece82
Pignataro67 <Pignataro67@gmail.com>
2021-05-07 04:00:22 -0700

    classes practice

--- app.js
+++ app.js
@@ -1,27 +1,16 @@
-const personProtypes = {
-  greeting: function() {
+class Person {
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  greeting() {
     return `Hello there ${this.firstName} ${this.lastName}`;
-  },
-  getsMarried: function(newLastName) {
-    this.lastName = newLastName;
   }
 }
 
-const mary = Object.create(personProtypes);
-mary.firstName = 'Mary';
-mary.lastName = 'William';
-mary.age = 30;
+const mary = new Person('Mary', 'Williams', '11-13-1980');
 
 mary.getsMarried('Thompson');
 
-console.log(mary.greeting());
-
-const pat = Object.create(personProtypes, {
-  firstName: {value: 'Pat'},
-  lastName: {value: 'Debaadeo'},
-  age: {value: 42}
-});
-
-console.log(pat);
-
-console.log(pat.greeting());
+console.log(mary.addNumbers(1, 2));
