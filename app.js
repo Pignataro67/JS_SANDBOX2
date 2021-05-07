bcfc2a18f124ea194dafbb41bef2f2ffae3cb01e
Pignataro67 <Pignataro67@gmail.com>
2021-05-07 04:11:25 -0700

    sub-classes practice

--- app.js
+++ app.js
@@ -1,33 +1,23 @@
 class Person {
   constructor(firstName, lastName) {
-    this.firstName = firstName;
+    this.firstName = firsrtName;
     this.lastName = lastName;
-    this .birthday = new Date(dob);
   }
 
   greeting() {
     return `Hello there ${this.firstName} ${this.lastName}`;
   }
+}
 
-  calculateAge() { 
-    const diff = Date.now() - this.birthday.getTime(); 
-    const ageDate = new Date(diff); 
-    return Math.abs(ageDate.getUTCFullYear() - 1970); 
-  }
+class Customer extends Person {
+  constructor(firstName, lastName, phone, membership){
+    super(firstName, lastName);
 
-  getsMarried(newLastName) { 
-    this.lastName = newLastName; 
-  }
-
-  static addNumbers(x, y) {  
-    return x + y; 
+    this.phone = phone;
+    this.membership = membership;  
   }
 }
 
-const mary = new Person('Mary', 'Williams', '11-13-1980');
+const john = new Customer('John', 'Doe', '555-555-5555', 'Standard');
 
-mary.getsMarried('Thompson');
-
-console.log(mary.addNumbers(1, 2));
-
-console.log(Person.addNumbers(1, 2));
+console.log(john);
