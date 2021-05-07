d1abd47ab9461dca7a9a08bcab500197d4f8917b
Pignataro67 <Pignataro67@gmail.com>
2021-05-07 04:03:04 -0700

    more classes practice

--- app.js
+++ app.js
@@ -2,10 +2,25 @@ class Person {
   constructor(firstName, lastName) {
     this.firstName = firstName;
     this.lastName = lastName;
+    this .birthday = new Date(dob);
   }
 
   greeting() {
     return `Hello there ${this.firstName} ${this.lastName}`;
+  }
+
+  calculateAge() { 
+    const diff = Date.now() - this.birthday.getTime(); 
+    const ageDate = new Date(diff); 
+    return Math.abs(ageDate.getUTCFullYear() - 1970); 
+  }
+
+  getsMarried(newLastName) { 
+    this.lastName = newLastName; 
+  }
+
+  static addNumbers(x, y) {  
+    return x + y; 
   }
 }
 
@@ -14,3 +29,5 @@ const mary = new Person('Mary', 'Williams', '11-13-1980');
 mary.getsMarried('Thompson');
 
 console.log(mary.addNumbers(1, 2));
+
+console.log(Person.addNumbers(1, 2));
