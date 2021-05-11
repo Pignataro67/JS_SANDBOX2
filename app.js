e0bb503e6ebd5d41ca42fb2a590b6996da25019e
Pignataro67 <Pignataro67@gmail.com>
2021-05-11 04:17:27 -0700

    adding function, more variables, debugging and... we are logging!

--- app.js
+++ app.js
@@ -6,15 +6,28 @@ function Book(title, author, isbn) {
 }
 
 // UI Constructor
-function UI() {
+function UI() {}
 
+// Add Book to List
+  UI.prototype.addBookToList = function(book) {
+    console.log(book);
 }
 
 // Event Listeners
 document.getElementById('book-form').addEventListener('submit', function(e) {
+  // get form values
   const title = document.getElementById('title').value,
-  author = document.getElementById('author').value,
-  isbn = document.getElementById('isbn').value
+        author = document.getElementById('author').value,
+        isbn = document.getElementById('isbn').value
+
+  // Instantiate book
+  const book = new Book(title, author, isbn);
+
+  // Instantiate UI
+  const ui = new UI();
+
+  // Add book to list
+  ui.addBookToList(book);
 
   e.preventDefault();
 })
