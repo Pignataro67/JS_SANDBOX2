2ec218b5cfddaef4dd44553f5960dec23e6ed06e
Pignataro67 <Pignataro67@gmail.com>
2021-05-12 05:12:55 -0700

    adding insert alert and timeout

--- app.js
+++ app.js
@@ -29,9 +29,20 @@ UI.prototype.showAlert = function(message, className) {
   // Create div
   const div = document.createElement('div');
   // Add classes
-  div.className = `alert ${className}` :
+  div.className = `alert ${className}`;
   // Add text
   div.appendChild(document.createTextNode(message));
+  // Get parent
+  const container = document.querySelector('container');
+  // Get form
+  const form = document.querySelector('#book-form');
+  // Insert alert
+  container.insertBefore(div, form);
+
+  // Timeout for 3 sec
+  setTimeout(function() {
+    document.querySelector('.alert').remove;
+  }, 3000);
 }
 
 // Clear Fields
