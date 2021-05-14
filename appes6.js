db7c351b1f8bb9df82725064e0e3796da8ec0699
Pignataro67 <Pignataro67@gmail.com>
2021-05-14 04:37:22 -0700

    adding eventListeners, submit, ui

--- appes6.js
+++ appes6.js
@@ -54,3 +54,48 @@ class UI {
     document.getElementById('isbn').value = '';
   }
 }
+
+// Event Listener for add book
+document.getElementById('book-form').addEventListener('submit', function(e) {
+  // get form values
+  const title = document.getElementById('title').value,
+        author = document.getElementById('author').value,
+        isbn = document.getElementById('isbn').value
+  
+    // Instantiate book
+    const book = new Book(title, author, isbn);
+  
+    // Instantiate UI
+    const ui = new UI();
+  
+    // Validate
+    if(title === '' || author === '' || isbn === '') {
+    // Error alert
+    ui.showAlert('Please fill in all fields', 'error');
+    } else {
+    // Add book to list
+    ui.addBookToList(book);
+  
+    // Show success
+    ui.showAlert('Book Added!', 'success');
+  
+    // Clear fields
+    ui.clearFields();
+  }
+      
+    e.preventDefault();
+  })
+  
+  // Event Listener to delete
+  document.getElementById('book-list').addEventListener('click', function(e) {
+  
+  // Instantiate UI
+  const ui = new UI();
+  
+  ui.deleteBook(e.target);
+  
+  // Show message
+  ui.showAlert('Book Removed', 'success');
+  
+  e.preventDefault();
+});
