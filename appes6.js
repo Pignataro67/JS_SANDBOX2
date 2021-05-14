ca578dc17281552ea354028f327dc9342567623b
Pignataro67 <Pignataro67@gmail.com>
2021-05-14 04:29:11 -0700

    showAlert function

--- appes6.js
+++ appes6.js
@@ -23,7 +23,23 @@ class UI {
   }
 
   showAlert(message, className) {
-    
+    // Create div
+    const div = document.createElement('div');
+    // Add classes
+    div.className = `alert ${className}`;
+    // Add text
+    div.appendChild(document.createTextNode(message));
+    // Get parent
+    const container = document.querySelector('.container');
+    // Get form
+    const form = document.querySelector('#book-form');
+    // Insert alert
+    container.insertBefore(div, form);
+
+    // Timeout for 3 sec
+    setTimeout(function() {
+      document.querySelector('.alert').remove();
+    }, 3000);
   }
 
   deleteBook(target) {
