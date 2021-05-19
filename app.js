106fe594718e41e6bac2280e680e58126bfc6f12
Pignataro67 <Pignataro67@gmail.com>
2021-05-19 04:49:08 -0700

    anotha' one, function

--- app.js
+++ app.js
@@ -1,50 +1,15 @@
-document.getElementById('button').addEventListener('click', loadData);
+document.getElementById('button1').addEventListener('click', loadCustomer);
 
-function loadData() {
-  // Create an XHR Object
+function loadCustomer(e) {
   const xhr = new XMLHttpRequest();
 
-  // OPEN
-  xhr.open('GET', 'data.txt', true);
-
-  // console.log('READYSTATE', xhr.readyState);
-
-  // Optional - Used for spinners/loaders
-  xhr.onprogress = function() {
-    console.log('READYSTATE', xhr)
-  }
+  xhr.open('GET', 'customer.json', true);
 
   xhr.onload = function() {
-    console.log('READYSTATE', xhr.readyState);
     if(this.status === 200) {
-      // console.log(this.responseText);
-      document.getElementById('output').innerHTML = `<h1>${this.responseText}</h1>`;
+      console.log(this.responseText);
     }
   }
 
-  // xhr.onreadystatechange = function() {
-  //   console.log('READYSTATE', xhr.readyState);
-  //   if(this.status === 200 && this.readyState === 4) {
-  //     console.log(this.responseText);
-  //   }
-  // }
-  
-  xhr.onerror = function() {
-    console.log('Request error...');
-  }
-
   xhr.send();
-
-  // readyState Values
-  // 0: request not initialized
-  // 1: server connection established
-  // 2: request received
-  // 3: processing request
-  // 4: request finished and response is ready
-
-
-  // HTTP Statuses
-  // 200: "OK"
-  // 403: "Forbidden"
-  // 404: "Not Found"
 }
