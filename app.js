c7a8102ad109331dc50b476607d15353306a9afb
Pignataro67 <Pignataro67@gmail.com>
2021-05-19 04:33:04 -0700

    adding a function, error reporting

--- app.js
+++ app.js
@@ -7,22 +7,33 @@ function loadData() {
   // OPEN
   xhr.open('GET', 'data.txt', true);
 
-  console.log('READYSTATE', xhr.readyState);
+  // console.log('READYSTATE', xhr.readyState);
+
+  // Optional - Used for spinners/loaders
+  xhr.onprogress = function() {
+    console.log('READYSTATE', xhr)
+  }
 
   xhr.onload = function() {
+    console.log('READYSTATE', xhr.readyState);
     if(this.status === 200) {
-      console.log(this.responseText);
+      // console.log(this.responseText);
+      document.getElementById('output').innerHTML = `<h1>${this.responseText}</h1>`;
     }
   }
 
-  xhr.onreadystatechange = function() {
-    if(this.status === 200 && this.readyState === 4) {
-      console.log(this.responseText);
-    }
-  }
+  // xhr.onreadystatechange = function() {
+  //   console.log('READYSTATE', xhr.readyState);
+  //   if(this.status === 200 && this.readyState === 4) {
+  //     console.log(this.responseText);
+  //   }
+  // }
   
-  xhr.send();
+  xhr.onerror = function() {
+    console.log('Request error...');
+  }
 
+  xhr.send();
 
   // readyState Values
   // 0: request not initialized
