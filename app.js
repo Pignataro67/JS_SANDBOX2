db0fc0317fb05704cf8f586f0d3d254e37d9288b
Pignataro67 <Pignataro67@gmail.com>
2021-05-20 09:12:26 -0700

    adding customers

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 document.getElementById('button1').addEventListener('click', loadCustomer);
 
+document.getElementById('button1').addEventListener('click', loadCustomer);
+
+// Load Customer
 function loadCustomer(e) {
   const xhr = new XMLHttpRequest();
 
@@ -7,7 +10,53 @@ function loadCustomer(e) {
 
   xhr.onload = function() {
     if(this.status === 200) {
-      console.log(this.responseText);
+      // console.log(this.responseText);
+
+      const customer = JSON.parse(this.responseText);
+
+      const output = `
+      <ul>
+        <li>ID: ${user.id}</li>
+        <li>Name: ${customer.name}</li>
+        <li>Company: ${customer.company}</li>
+        <li>Phone: ${customer.phone}</li>
+      </ul>
+      `;
+
+      document.getElementById('customer').innerHTML = output;
+    }
+  }
+
+  xhr.send();
+}
+
+
+// Load Customers
+function loadCustomer(e) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.open('GET', 'customers.json', true);
+
+  xhr.onload = function() {
+    if(this.status === 200) {
+      // console.log(this.responseText);
+
+      const customers = JSON.parse(this.responseText);
+
+      let output = '';
+
+      customers.forEach(function(customer) {
+        output += `
+        <ul>
+          <li>ID: ${user.id}</li>
+          <li>Name: ${customer.name}</li>
+          <li>Company: ${customer.company}</li>
+          <li>Phone: ${customer.phone}</li>
+        </ul>
+      `;
+      });
+
+      document.getElementById('customer').innerHTML = output;
     }
   }
 
