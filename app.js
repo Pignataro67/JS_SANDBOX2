88f341abac90a4ae01780911689e5f1044160b2e
Pignataro67 <Pignataro67@gmail.com>
2021-05-20 09:13:42 -0700

    adding function

--- app.js
+++ app.js
@@ -29,3 +29,35 @@ function loadCustomer(e) {
 
   xhr.send();
 }
+
+// Load Customers
+function loadCustomers(e) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.open('GET', 'customers.json', true);
+
+  xhr.onload = function() {
+    if(this.status === 200) {
+      // console.log(this.responseText);
+
+      const customers = JSON.parse(this.responseText);
+
+      let output = '';
+
+      customers.forEach(function(customer) {
+        output += `
+        <ul>
+          <li>ID: ${user.id}</li>
+          <li>Name: ${customer.name}</li>
+          <li>Company: ${customer.company}</li>
+          <li>Phone: ${customer.phone}</li>
+        </ul>
+      `;
+      });
+
+      document.getElementById('customer').innerHTML = output;
+    }
+  }
+
+  xhr.send();
+}
