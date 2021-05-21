619fccf9f17eeacff38c37acf92981a0ea39c9b7
Pignataro67 <Pignataro67@gmail.com>
2021-05-21 05:29:10 -0700

    adding to the function

--- app.js
+++ app.js
@@ -3,7 +3,18 @@ document.querySelector('.get-jokes').addEventListener('click', getJokes);
 function getJokes(e) {
   const number = document.querySelector('input[type="number"]').value;
 
-  console.log(number);
+  const xhr = new XMLHttpRequest();
+
+  xhr.open(`http:api.icndb.com/jokes/random/${number}`, true);
+
+  xhr.onload = function() {
+    if(this.status === 200) {
+      const res = JSON.parse(this.responseText);
+      console.log(res);
+    }
+  }
+
+  xhr.send();
 
   e.preventDefault();
 }
