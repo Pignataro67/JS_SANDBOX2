fafe3a736dd71f022edad0499009638b7aa4d409
Pignataro67 <Pignataro67@gmail.com>
2021-05-24 07:32:15 -0700

    debugging spacing, adding a querySel.

--- app.js
+++ app.js
@@ -5,21 +5,23 @@ function getJokes(e) {
 
   const xhr = new XMLHttpRequest();
 
-  xhr.open(`http:api.icndb.com/jokes/random/${number}`, true);
+  xhr.open('GET', `http:api.icndb.com/jokes/random/${number}`, true);
 
   xhr.onload = function() {
     if(this.status === 200) {
-      const res = JSON.parse(this.responseText);
+      const response = JSON.parse(this.responseText);
       
       let output = '';
 
-      if(res.type === 'success') {
-        res.value.forEach(function(joke) {
-          output +=`<li>${joke.joke}</li>`
+      if(response.type === 'success') {
+        response.value.forEach(function(joke) {
+          output += `<li>${joke.joke}</li>`
         })
-      }else {
-        output += `<li>Something went wrong</li>`;
+      } else {
+        output += '<li>Something went wrong</li>';
       }
+
+      document.querySelector('.jokes').innerHTML = output;
     }
   }
 
