44d0d7ad243a7d63c457f933b7d9a5762cd70245
Pignataro67 <Pignataro67@gmail.com>
2021-05-26 04:39:42 -0700

    debugging, 'easy' v. 'not easy'

--- app.js
+++ app.js
@@ -1,22 +1,6 @@
-function easyHTTP() {
-  this.http = new XMLHttpRequest();
-}
+const http = new easyHTTP;
 
-// Make an HTTP GET Request
-easyHTTP.prototype.get = function(url) {
-  this.http.open('GET', url, true);
+// Get Posts
+http.get('https://jsonplaceholder.typicode.com/posts');
 
-  this.http.onload = function() {
-    if(this.http.status === 200) {
-      console.log(this.http.responseText);
-    }
-  }
-
-  this.http.send();
-}
-
-// Make an HTTP POST Request
-
-// Make an HTTP PUT Request
-
-// Make an HTTP DELETE Request
+console.log(posts);
