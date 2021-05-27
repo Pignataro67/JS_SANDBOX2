b02595dc093c119bea3da74cb679d558e635651a
Pignataro67 <Pignataro67@gmail.com>
2021-05-27 05:23:54 -0700

    Delete request

--- app.js
+++ app.js
@@ -24,11 +24,29 @@ const data = {
   body: 'This is a custom post'
 };
 
-Create Post
-http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, post) {
-  if (err) {
+// Create Post
+// http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, post) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// });
+
+// Update Post
+// http.put('https://jsonplaceholder.typicode.com/posts/1', data, function(err, post) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// })
+
+// Delete Post
+http.get('https://jsonplaceholder.typicode.com/posts/1', function(err, response) {
+  if(err) {
     console.log(err);
   } else {
-    console.log(err);
+    console.log(posts);
   }
 });
