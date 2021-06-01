42be61c165f146b3d617113baa652118f9d5079d
Pignataro67 <Pignataro67@gmail.com>
2021-06-01 06:03:08 -0700

    adding a function, more promise practice

--- app.js
+++ app.js
@@ -10,4 +10,14 @@ function createPost(post, callback) {
   }, 2000);
 }
 
+function getPosts() {
+  setTimeout(function() {
+    let output = '';
+    post.forEach(function(post) {
+      output += `<li>${post.title}</li>`;
+    });
+    document.body.innerHTML = output;
+  }, 1000);
+}
+
 createPost({title: 'Post Five', body: 'This is post five'}, getPosts);
