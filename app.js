889a6d44bd8976197bb85673adc10f839f4cad19
Pignataro67 <Pignataro67@gmail.com>
2021-06-02 04:58:35 -0700

    adding a function

--- app.js
+++ app.js
@@ -1,36 +1,11 @@
-const posts = [
-  {title: 'Post Three', body: 'This is post three'},
-  {title: 'Post Four', body: 'This is post four'}
-];
+document.getElementById('button1').addEventListener('click', getText);
 
-function createPost(post) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      posts.push(post);
-
-      const error = true;  // can be false, too
-
-      if(!error) {
-        resolve();
-      } else {
-        reject('Error: Something went wrong');
-      }
-    }, 2000);
-  });
-}
-
-function getPosts() {
-  setTimeout(function() {
-    let output = '';
-    post.forEach(function(post) {
-      output += `<li>${post.title}</li>`;
+function getText() {
+  fetch('test.txt')
+    .then(function(res) {
+      return res.text();
+    })
+    .then(function(data) {
+      console.log(data);
     });
-    document.body.innerHTML = output;
-  }, 1000);
 }
-
-createPost({title: 'Post Five', body: 'This is post five'})
-.then(getPosts)
-.catch(function(err) {
-  console.log(err);
-});
