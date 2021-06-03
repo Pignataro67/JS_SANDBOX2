0404130e9f293a6c19d3cfc826382329618e6ca0
Pignataro67 <Pignataro67@gmail.com>
2021-06-03 07:55:16 -0700

    more arrow function practice

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
-const sayHello = function() {
-  console.log('Hello');
-}
+// const sayHello = function() {
+//   console.log('Hello');
+// }
 
 const sayHello = () => {
   console.log('Hello');
 }
 
-sayHello();
+// One line function does not need braces
+const sayHello = () => console.log('Hello');
+
+// One line return
+const sayHello = () => 'Hello';
+
+// Same as above
+const sayHello = function() {
+  return 'Hello';
+}
+
+const sayHello = () => 'Hello';
+
+console.log(sayHello());  
