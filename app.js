58219e166b24e2396b52082cf344f03764ea95b9
Pignataro67 <Pignataro67@gmail.com>
2021-06-03 07:32:28 -0700

    fetch practice

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ document.getElementById('button1').addEventListener('click', getText);
 
 document.getElementById('button2').addEventListener('click', getJson);
 
+document.getElementById('button3').addEventListener('click', getExternal);
+
 // Get local text file
 function getText() {
   fetch('test1.txt')
@@ -25,6 +27,30 @@ function getJSON() {
   })
   .then(function(data) {
     console.log(data);
+    let output = '';
+    data.forEach(function(post) {
+      output += `<li>${post.title}</li>`;
+    });
+    document.getElementById('output').innerHTML = output;
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
+}
+
+// Get from external API
+function getExternal() {
+  fetch('https://api.github.com/users')
+  .then(function(res) {
+    return res.json();
+  })
+  .then(function(data) {
+    console.log(data);
+    let output = '';
+    data.forEach(function(user) {
+      output += `<li>${post.title}</li>`;
+    });
+    document.getElementById('output').innerHTML = output;
   })
   .catch(function(err) {
     console.log(err);
