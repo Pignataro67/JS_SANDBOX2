c5e5f5416e95ed8e2777ff4799e9866ad26a1ee3
Pignataro67 <Pignataro67@gmail.com>
2021-06-04 05:11:17 -0700

    changing to arrow functions in previous fetch exercise

--- app.js
+++ app.js
@@ -43,3 +43,54 @@ const nameLengths = users.map((name) => {
 const nameLengths = users.map(name => name.length);
 
 console.log(nameLengths);
+
+
+
+///// Using Arrow Functions in the previous Fetch exercise
+
+document.getElementById('button1').addEventListener('click', getText);
+document.getElementById('button2').addEventListener('click', getJson);
+document.getElementById('button3').addEventListener('click', getExternal);
+
+// Get local text file
+function getText() {
+  fetch('test.txt')
+    .then(res => res.text())
+    .then(data => {
+      console.log(data);
+      document.getElementById('output').innerHTML = data;
+    })
+    .catch(err => console.log(err));
+}
+
+// Get local json data
+function getJSON() {
+  fetch('posts.json')
+  .then(res => res.json())
+  .then(data => {
+    console.log(data);
+    let output = '';
+    data.forEach(function(post) {
+      output += `<li>${post.title}</li>`;
+    });
+    document.getElementById('output').innerHTML = output;
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
+}
+
+// Get from external API
+function getExternal() {
+  fetch('https://api.github.com/users')
+  .then()
+  .then(data => {
+    console.log(data);
+    let output = '';
+    data.forEach(function(user) {
+      output += `<li>${post.title}</li>`;
+    });
+    document.getElementById('output').innerHTML = output;
+  })
+  .catch(err => console.log(err));
+}
