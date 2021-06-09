e6035e749885762c36c05b9cc83f065d5de58e66
Pignataro67 <Pignataro67@gmail.com>
2021-06-09 04:00:53 -0700

    more fetch with promises

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const http = new EasyHTTP;
 
 // Get Users
-http.get('https://jsonplaceholder.typicode.com/uers')
+// http.get('https://jsonplaceholder.typicode.com/uers')
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
+
+// User Data
+const data = {
+  name: 'John Doe',
+  username: 'johndoe',
+  email: 'jdoe@gmail.com'
+}
+
+// Create Post
+http.get('https://jsonplaceholder.typicode.com/uers', data)
   .then(data => console.log(data))
   .catch(err => console.log(err));
