9c75965ad1647524051209939b29b687814b9f07
Pignataro67 <Pignataro67@gmail.com>
2021-06-10 04:42:11 -0700

    more async/await practice

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
-async function myFunc() {
-  const promise = new Promise((resolve, reject) => {
-    setTimeout(() => resolve('Hello'), 1000);
-  });
+// async function myFunc() {
+//   const promise = new Promise((resolve, reject) => {
+//     setTimeout(() => resolve('Hello'), 1000);
+//   });
 
-  const error = false;
+//   const error = false;
 
-  if(!error) {
-    const res = await promise; // wait until promise is resolved
-    return res;
-  } else {
-    await Promise.reject(new Error('Something went wrong'));
-  }
+//   if(!error) {
+//     const res = await promise; // wait until promise is resolved
+//     return res;
+//   } else {
+//     await Promise.reject(new Error('Something went wrong'));
+//   }
+// }
+
+// myFunc()
+//   .then(res => console.log(res));
+//   .catch(err => console.log(err));
+
+async function getUsers() {
+  // await response of the fetch call
+  const response = await fetch('https://jsonPlaceholder.typicode.com/users');
+
+  // Only proceed once its resolved
+  const data = await response.json();
+
+  // Only proceed once second promise is resolved
+  return data;
 }
 
-myFunc()
-  .then(res => console.log(res));
-  .catch(err => console.log(err));
+getUsers().then(users => console.log(users));
