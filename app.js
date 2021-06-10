d2d6b69842f86ed38b40386a0e7402c264fd4238
Pignataro67 <Pignataro67@gmail.com>
2021-06-10 04:36:37 -0700

    more async/await practice

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 async function myFunc() {
-  return "Hello";
-}
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => resolve('Hello'), 1000);
+  });
 
-console.log(myFunc());
+  const error = false;
+
+  if(!error) {
+    const res = await promise; // wait until promise is resolved
+    return res;
+  } else {
+    await Promise.reject(new Error('Something went wrong'));
+  }
+}
 
 myFunc()
   .then(res => console.log(res));
+  .catch(err => console.log(err));
