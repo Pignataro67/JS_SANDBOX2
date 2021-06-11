e971fbc8c65867bc4ad0091bd4ab0ab90a0b83ba
Pignataro67 <Pignataro67@gmail.com>
2021-06-11 04:30:20 -0700

    adding functions

--- easyhttp3.js
+++ easyhttp3.js
@@ -19,4 +19,36 @@ class EasyHTTP {
       .catch(err => reject(err));
     });
   }
+
+// Make an HTTP POST Request
+post(url, data) {
+    return new Promise((resolve, reject) => {
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    }) 
+      .then(res => res.json())
+      .then(data => resolve(data))
+      .catch(err => reject(err));
+    });
+  }
+
+  // Make a HTTP PUT Request
+  put(url, data) {
+    return new Promise((resolve, reject) => {
+    fetch(url, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    }) 
+      .then(res => res.json())
+      .then(data => resolve(data))
+      .catch(err => reject(err));
+    });
+  }
 }
