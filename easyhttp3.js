67f0e45d1e8c2a0585f863560d06577bebdf725c
Pignataro67 <Pignataro67@gmail.com>
2021-06-16 05:20:27 -0700

    adding async to post, refractoring

--- easyhttp3.js
+++ easyhttp3.js
@@ -18,20 +18,19 @@ class EasyHTTP {
   }
 
 // Make an HTTP POST Request
-post(url, data) {
-    return new Promise((resolve, reject) => {
-    fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-type': 'application/json'
-      },
-      body: JSON.stringify(data)
-    }) 
-      .then(res => res.json())
-      .then(data => resolve(data))
-      .catch(err => reject(err));
-    });
-  }
+async post(url, data) {
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  }); 
+  
+  const resData = await response.json();
+  return resData;
+
+}
 
   // Make a HTTP PUT Request
   put(url, data) {
