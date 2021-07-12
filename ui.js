b7da8c832cb0fe28b2ec30aa492f03d9478c344b
Pignataro67 <Pignataro67@gmail.com>
2021-07-12 05:57:25 -0700

    adding to ui

--- ui.js
+++ ui.js
@@ -11,8 +11,18 @@ class UI {
             <img class="img-fluid mb-2" src="${user.avatar_url}">
             <a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block">View Profile</a>
           </div>
-          <div class="col-md-3">
-
+          <div class="col-md-9">
+            <span class="badge badge-primary">Public Repos:${user.public_repos}</span>
+            <span class="badge badge-primary">Public Gists:${user.public_gists}</span>
+            <span class="badge badge-primary">Followers:${user.followers}</span>
+            <span class="badge badge-primary">Public Repos:${user.following}</span>
+            <br><br>
+            <ul class="list-group">
+              <li class="list-group-item">Company: ${user.company}</li>
+              <li class="list-group-item">Website/Blog: ${user.blog}</li>
+              <li class="list-group-item">Location: ${user.company}</li>
+              <li class="list-group-item">Member Service: ${user.created_at}</li>
+            </ul>
           </div>
         </div>
       </div>
