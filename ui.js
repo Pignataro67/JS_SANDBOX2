4be0b23764fb66a87c84783c371cf977f7cfe720
Pignataro67 <Pignataro67@gmail.com>
2021-07-12 06:22:37 -0700

    adding function

--- ui.js
+++ ui.js
@@ -3,6 +3,7 @@ class UI {
     this.profile = document.getElementById('profile');
   }
 
+  // Display profile in UI
   showProfile(user) {
     this.profile.innerHTML = `
       <div class="card card-body mb-3">
@@ -30,7 +31,19 @@ class UI {
       <div id="repos"></div>
     `;
   }
-  
+
+  // Show Alert
+  showAlert(message, className) {
+    // Create div
+    const div = document.createElement('div');
+    // Add classes
+    div.className = className;
+    // Add text
+    div.appendChild(document.createTextNode(message));
+    // Get parent
+    const container = document.querySelector('.searchContainer');
+  } 
+
   clearProfile() {
     this.profile.innerHTML = '';
   }
